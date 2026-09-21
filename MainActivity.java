48f09861f7cc3fc7db87ddb6e57d693d5652a57b
ZIGOBOX <adelin.vignal.running@outlook.fr>
2026-09-21 19:06:12 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,66 @@
+package fr.zigobox.suite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    // Si ton dépôt GitHub Pages porte un autre nom, change seulement cette ligne.
+    private static final String START_URL = "https://zigobox.github.io/ZiGoBox-Suite/";
+    private static final String FALLBACK_URL = "file:///android_asset/index.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setAllowFileAccess(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            private boolean fallbackLoaded = false;
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                if (request.isForMainFrame() && !fallbackLoaded) {
+                    fallbackLoaded = true;
+                    view.loadUrl(FALLBACK_URL);
+                }
+            }
+        });
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(START_URL);
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
